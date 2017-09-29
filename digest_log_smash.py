1c12f386130834b93f95922bd30d5f7050892c45
Paul <paultimo@gmail.com>
2017-09-28 20:02:48 -0600

    Added Context Help

--- digest_log_smash.py
+++ digest_log_smash.py
@@ -1,98 +1,90 @@
+import sys
+import getopt
 import hashlib
 import re
-import sys
-import argparse
 
-# Add Help Context
-parser = argparse.ArgumentParser(description='Digest Smash: BruteForce http.authorization & user:realm against passFile.', epilog="Be Kind, Please Rewind")
-parser.add_argument('logfile', help='a file containing single line http.authorization captures')
-parser.add_argument('passfile', help='a file containing single line passwords')
-args = parser.parse_args()
+# Global Variables
+i = 0
 
 # Usage
 def usage():
     print "Digest_Smash"
     print 
-    print "Usage: digest_log_smash.py <log file> <pass file>"
-    print
-    print 
+    print "Usage: digest_log_smash.py -e <log file> <pass file>"
+    print " -l --log=log file_to_run - load log file"
+    print " -p --pass=pass file_to_run - load pass file"
     print "Examples: "
-    print "digest_log_smash.py testlog.log pass_table.txt"
+    print "digest_log_smash.py -l testlog.log -p pass.txt"
     sys.exit(0)
 
-try:
+# Main Program
+def main():
+  global i
 
- # Variables
- i = 0
- file_name = sys.argv[1]
- file_pass = sys.argv[2]
+  if not len(sys.argv[1:]):
+    usage()
 
- # Open File and Build RegEx strings
- with open(file_name) as l:
-  for line in l:
-    i=i + 1
-    chk = 0
-    sUser = re.search('username=\"([^\"]*)\"', line)
-    sRealm = re.search('realm=\"([^\"]*)\"', line)
-    sUri = re.search('uri=\"([^\"]*)\"', line)
-    sNonce = re.search('nonce=\"([^\"]*)\"', line)
-    sNc = re.search('nc=\"([^\"]*)\"', line)
-    if not hasattr(sNc, 'group'):
-      sNc = re.search('nc=([^,]*),', line)
-    sCnonce = re.search('cnonce=\"([^\"]*)\"', line)
-    sQop = re.search('qop=\"([^\"]*)\"', line)
-    if not hasattr(sQop, 'group'):
-      sQop = re.search('qop=([^,]*),', line)
-    sResponse = re.search('response=\"([^\"]*)\"',line)
+  # read the commandline options
+  try:
+    opts, args = getopt.getopt(sys.argv[1:], "hl:p:", ["help", "log", "pass"])
+  except getopt.GetoptError as err:
+    print str(err)
+    usage()
 
-    #print sUser.group(1)
-    #print sRealm.group(1)
-    #print sUri.group(1)
-    #print sNonce.group(1)
-    #print sNc.group(1)
-    #print sCnonce.group(1)
-    #print sQop.group(1)
-    #print sResponse
+  for o,a in opts:
+    if o in ("-h", "--help"):
+      usage()
+    elif o in ("-l", "--log"):
+      file_name = a
+    elif o in ("-p", "--pass"):
+      file_pass = a
+    else:
+      assert False, "Unhandled Option"
 
+  try:
+    print file_name
+    # Open File and Build RegEx strings
+    with open(file_name) as l:
+      for line in l:
+        i=i + 1
+        chk = 0
+        sUser = re.search('username=\"([^\"]*)\"', line)
+        sRealm = re.search('realm=\"([^\"]*)\"', line)
+        sUri = re.search('uri=\"([^\"]*)\"', line)
+        sNonce = re.search('nonce=\"([^\"]*)\"', line)
+        sNc = re.search('nc=\"([^\"]*)\"', line)
+        if not hasattr(sNc, 'group'):
+          sNc = re.search('nc=([^,]*),', line)
+        sCnonce = re.search('cnonce=\"([^\"]*)\"', line)
+        sQop = re.search('qop=\"([^\"]*)\"', line)
+        if not hasattr(sQop, 'group'):
+          sQop = re.search('qop=([^,]*),', line)
+        sResponse = re.search('response=\"([^\"]*)\"',line)
 
-
-    HA2 = hashlib.md5("GET:" + sUri.group(1)).hexdigest()
-    sStack = sNonce.group(1)+":"+sNc.group(1)+":"+sCnonce.group(1)+":"+sQop.group(1)
+   
+        HA2 = hashlib.md5("GET:" + sUri.group(1)).hexdigest()
+        sStack = sNonce.group(1)+":"+sNc.group(1)+":"+sCnonce.group(1)+":"+sQop.group(1)
    
 
-    print str(i) + ": --> " + sUser.group(1) +":"+ sRealm.group(1),
-    # Open password file and test
-    with open(file_pass) as f:
-      for line in f:
-        my_str = sUser.group(1) + ":" + sRealm.group(1) + ":" + line.rstrip()
-        HA1 = hashlib.md5(my_str).hexdigest()
+        print str(i) + ": --> " + sUser.group(1) +":"+ sRealm.group(1),
+        # Open password file and test
+        with open(file_pass) as f:
+          for line in f:
+            my_str = sUser.group(1) + ":" + sRealm.group(1) + ":" + line.rstrip()
+            HA1 = hashlib.md5(my_str).hexdigest()
         
-        # Check reponse hash
-        response = hashlib.md5(HA1 + ":" + sStack + ":" + HA2).hexdigest()
-        if sResponse.group(1) == response:
-          print " [PASS] :-  %s:%s" % (sUser.group(1),line.rstrip())
-          chk = 1
-          break
-    f.close
-    if chk == 0:
-      print " [FAIL] "
- l.closed
+            # Check reponse hash
+            response = hashlib.md5(HA1 + ":" + sStack + ":" + HA2).hexdigest()
+            if sResponse.group(1) == response:
+              print " [PASS] :-  %s:%s" % (sUser.group(1),line.rstrip())
+              chk = 1
+              break
+        f.close
+        if chk == 0:
+          print " [FAIL] "
+    l.closed
+  
+  except Exception as e:
+    print str(e)
 
-except:
- print "[!!] Failed to Parse Log File %s" % (file_name)
- if not hasattr(sUser, 'group'):
-  print "[!!] %s: username is structured wrong in file." % (str(i))
- if not hasattr(sRealm, 'group'):
-  print "[!!] %s: realm is structured wrong in file." % (str(i))
- if not hasattr(sUri, 'group'):
-  print "[!!] %s: uri is structured wrong in file." % (str(i))
- if not hasattr(sNonce, 'group'):
-  print "[!!] %s: nonce is structured wrong in file." % (str(i))
- if not hasattr(sNc, 'group'):
-  print "[!!] %s: nc is structured wrong in file." % (str(i))
- if not hasattr(sCnonce, 'group'):
-  print "[!!] %s: cnonce is structured wrong in file." % (str(i))
- if not hasattr(sQop, 'group'):
-  print "[!!] %s: qop is structured wrong in file." % (str(i))
- if not hasattr(sResponse, 'group'):
-  print "[!!] %s: response is structured wrong in file." % (str(i))
+main()
